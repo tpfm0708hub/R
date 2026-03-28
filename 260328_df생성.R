a86e8400c67ed239be95aecdb1199ef6a9078798
tpfm0708hub <tpfm0708@gmail.com>
2026-03-28 14:26:29 +0900

    260328_df생성.R
    Updated data frame creation with sample data and improved variable names.

--- 260328_df생성.R
+++ 260328_df생성.R
@@ -1,16 +1,19 @@
 rm(list=ls())
 library(dplyr)
 
+#  임의의 표본 500개 생성
 idx_01<-c(
   1:500
   )
+#  주소구성: "수정구", "분당구", "중원구"
 address_01<-sample(
   c("수정구","분당구","중원구"),size=length(idx_01),replace=TRUE
   )
+#  수입범위: 240~500
 income_01<-sample(
   c(240:500),size=length(idx_01),replace=TRUE
   )
 
 df_01<-data.frame(
-  순번=idx_01,주소=address_01,수입=income_01
+  식별=idx_01,주소=address_01,수입=income_01
   )
