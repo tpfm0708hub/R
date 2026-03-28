1e972f4910ef8bcfbb8f36b8523e23cdd755334e
tpfm0708hub <tpfm0708@gmail.com>
2026-03-28 09:00:43 +0900

    Add files via upload

---
+++ 260328_df생성.R
@@ -0,0 +1,16 @@
+rm(list=ls())
+library(dplyr)
+
+idx_01<-c(
+  1:500
+  )
+address_01<-sample(
+  c("수정구","분당구","중원구"),size=500,replace=TRUE
+  )
+income_01<-sample(
+  c(240:500),size=500,replace=TRUE
+  )
+
+df_01<-data.frame(
+  순번=idx_01,주소=address_01,수입=income_01
+  )
