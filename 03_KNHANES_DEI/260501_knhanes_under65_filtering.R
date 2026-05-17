082fee8958e1d7307c6dfd3963a0aa2799cb8279
SEHYEON JANG <tpfm0708@gmail.com>
2026-05-17 10:59:01 +0900

    KNHANES 제9기 65세 미만 필터링
    제9기(2022-2024) KNHANES (국민건강영양조사)에서 당뇨병 의사진단 여부 변수(DE1_dg)에 응답한 분석 집단에서 65세 미만 대상자만을 추출합니다.

---
+++ 03_KNHANES_DEI/260501_knhanes_under65_filtering.R
@@ -0,0 +1,44 @@
+rm(list=ls())
+library(fs)# dir_ls()
+library(dplyr)
+library(haven)
+library(doParallel)#  foreach()
+library(psych)# describe()
+# 활용자료: "국민건강영양조사 제9기(2022-2024)
+# URL: https://knhanes.kdca.go.kr/knhanes/rawDataDwnld/rawDataDwnld.do
+
+# 병렬처리 코어 설정
+cl <- makeCluster(parallel::detectCores()-1)
+registerDoParallel(cl)
+
+# 데이터 폴더 경로 설정
+path_001<-'D:/github'
+# 경로 내 hn으로 시작하는 SAS 파일 목록 추출
+df_list_01<-dir_ls(path_001, regexp="hn.*\\.sas7bdat$")
+
+# 해당 SAS 파일을 병렬로 불러온 뒤 결합
+df_001<-foreach(f=df_list_01, .combine=bind_rows, .packages=c('dplyr', 'haven'))%dopar%{
+  df<-read_sas(f)
+  return(df)}
+
+# 병렬처리 클러스터 종료
+stopCluster(cl)
+
+# 당뇨병 의사진단 여부(DE1_dg) 없음(0), 있음(1) 외 제거  
+df_002 <- df_001 %>% filter(DE1_dg %in% c(0, 1))
+
+# 연령 분포 확인
+#df_002 %>% select(age) %>% describe(.)
+
+# 노인 외 65세 미만 집단 추출
+df_003 <- df_002 %>% filter(age < 65)
+
+# 노인 제외 전후 표본 수 변화 확인
+library(formattable)
+
+print(
+  paste0('노인제외 전 표본수 : ', comma(nrow(df_002),digits=0), '명 / ',
+         '노인제외 후 표본수 : ', comma(nrow(df_003),digits=0), '명')
+)
+# 출력결과
+#[1] "노인제외 전 표본수 : 17,255명/노인제외 후 표본수 : 11,805명"
